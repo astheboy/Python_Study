5fabe7209d68301c4645394db9a82f31097d5c39
astheoby <astheboy@outlook.com>
2023-05-19 10:32:23 +0900

    v1.1

--- python_automate/extract_table_to_pdf.py
+++ python_automate/extract_table_to_pdf.py
@@ -1,9 +1,14 @@
 import tabula
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+pdf_filename = "table.pdf"
+relative_path = os.path.join(script_dir, pdf_filename)
+absolute_path = os.path.abspath(relative_path)
+
+print(relative_path)
+print(absolute_path)
 
 # Extract table from pdf
-df = tabula.read_pdf(
-    "/Users/hanseungryun/Library/CloudStorage/OneDrive-개인/문서/python_study/python_automate/input/table.pdf",
-    pages="1",
-)
-
+df = tabula.read_pdf(relative_path, pages="1", multiple_tables=True)
 print(df)
